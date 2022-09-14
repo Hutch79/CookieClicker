75071574a97fb57e118607f63123119c0c0de50c
Hutch79 <andrin.monn@gmail.com>
2022-09-14 08:04:44 +0200

    added Metrics (bStats)

--- src/main/java/ch/hutch79/cookieclicker/Main.java
+++ src/main/java/ch/hutch79/cookieclicker/Main.java
@@ -1,6 +1,7 @@
 package ch.hutch79.cookieclicker;
 
 import ch.hutch79.cookieclicker.util.GuiListerne;
+import ch.hutch79.cookieclicker.util.Metrics;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -13,6 +14,9 @@ public final class Main extends JavaPlugin{
         Bukkit.getConsoleSender().sendMessage(getConfig().getString("prefix") + " §6CookieClicker §bby Hutch79");
         Bukkit.getConsoleSender().sendMessage(getConfig().getString("prefix") + " §7Has been §2Enabled");
         Bukkit.getConsoleSender().sendMessage(getConfig().getString("prefix") + "------------------------------------------");
+
+        int pluginId = 16433; // <-- Replace with the id of your plugin!
+        Metrics metrics = new Metrics(this, pluginId);
 
         getCommand("cookieclicker").setExecutor(new CookieClickerCommand(this));
 
