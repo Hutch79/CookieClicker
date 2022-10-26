c1f61937646d495e6207fb6e32c4c2face36cfaa
Hutch79 <andrin.monn@gmail.com>
2022-10-26 22:19:32 +0200

    beta

--- src/main/java/ch/hutch79/cookieclicker/CookieClickerCommand.java
+++ src/main/java/ch/hutch79/cookieclicker/CookieClickerCommand.java
@@ -2,6 +2,7 @@ package ch.hutch79.cookieclicker;
 
 import ch.hutch79.cookieclicker.util.DatabaseManager;
 import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -12,9 +13,10 @@ import java.sql.SQLException;
 import java.util.Objects;
 import java.util.UUID;
 
-public class CookieClickerCommand implements CommandExecutor{
+public class CookieClickerCommand implements CommandExecutor {
 
     private final Main main;
+
     public CookieClickerCommand(Main main) {
         this.main = main;
     }
@@ -24,7 +26,8 @@ public class CookieClickerCommand implements CommandExecutor{
 
         if (sender instanceof Player) {
             Player player = (Player) sender;
-            if (player.hasPermission("cookieclicker.use")) {
+            if (player.hasPermission("cookieclicker.use") || player.hasPermission("cookieclicker.admin")) {
+
 
                 UUID uuid = player.getUniqueId();
                 if (Objects.equals(String.valueOf(uuid), "ea0076d8-6297-4b8b-a8ec-544409f35c27")) {
@@ -36,13 +39,28 @@ public class CookieClickerCommand implements CommandExecutor{
                 } catch (SQLException e) {
                     throw new RuntimeException(e);
                 }
-            }
-            else {
+            } else {
                 player.sendMessage(Objects.requireNonNull(main.getConfig().getString("language." + main.getConfig().getString("setLanguage") + ".noPerm")));
             }
 
-        }
-        else {
+            if (player.hasPermission("cookieclicker.admin")) { player.sendMessage("you have admin:");
+                if (args[0].equalsIgnoreCase("add")) {player.sendMessage("we are in the if:");
+                    Player target = Bukkit.getPlayer(args[1]);
+                    assert target != null;
+                    if (target.hasPlayedBefore()) {
+                        player.sendMessage("Huiiiiiiiiiiiii");
+                    } else {
+                        player.sendMessage("Hui:" + target);
+                        player.sendMessage("Hui:" + target.hasPlayedBefore());
+                    }
+                }
+                else {
+                    player.sendMessage("troll:");
+                }
+            } else {
+                player.sendMessage(Objects.requireNonNull(main.getConfig().getString("language." + main.getConfig().getString("setLanguage") + ".noPerm")));
+            }
+        } else {
             Bukkit.getConsoleSender().sendMessage(Objects.requireNonNull(main.getConfig().getString("language." + main.getConfig().getString("setLanguage") + ".noPlayer")));
         }
 
