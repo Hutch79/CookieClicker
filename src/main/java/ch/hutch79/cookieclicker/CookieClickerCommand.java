10356131816bb6c934b1bf0a9d11549b993d06c6
Hutch79 <andrin.monn@gmail.com>
2022-10-28 06:40:04 +0200

    Delete of testing code. Needs to be done differently

--- src/main/java/ch/hutch79/cookieclicker/CookieClickerCommand.java
+++ src/main/java/ch/hutch79/cookieclicker/CookieClickerCommand.java
@@ -27,39 +27,11 @@ public class CookieClickerCommand implements CommandExecutor {
         if (sender instanceof Player) {
             Player player = (Player) sender;
             if (player.hasPermission("cookieclicker.use") || player.hasPermission("cookieclicker.admin")) {
-
-
-                UUID uuid = player.getUniqueId();
-                if (Objects.equals(String.valueOf(uuid), "ea0076d8-6297-4b8b-a8ec-544409f35c27")) {
-                    player.sendMessage("§d[§5Debug§d] UUID: " + player.getUniqueId());
-                }
                 try {
                     DatabaseManager.updateUser(player, 0.0, 0.0, 0, 0.0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
                     Gui.mainGui(player, 1);
                 } catch (SQLException e) {
                     throw new RuntimeException(e);
-                }
-            } else {
-                player.sendMessage(Objects.requireNonNull(main.getConfig().getString("language." + main.getConfig().getString("setLanguage") + ".noPerm")));
-            }
-
-            if (player.hasPermission("cookieclicker.admin")) { player.sendMessage("you have admin:");
-
-                if(args.lenght >= 1) {
-                    if (args[0].equalsIgnoreCase("add")) {player.sendMessage("we are in the if:");
-                        Player target = Bukkit.getPlayer(args[1]);
-                        assert target != null;
-                        if (target.hasPlayedBefore()) {
-                            player.sendMessage("Huiiiiiiiiiiiii");
-                        } else {
-                            player.sendMessage("Hui:" + target);
-                            player.sendMessage("Hui:" + target.hasPlayedBefore());
-                            
-                        }
-                    }
-                }
-                else {
-                    player.sendMessage("troll:");
                 }
             } else {
                 player.sendMessage(Objects.requireNonNull(main.getConfig().getString("language." + main.getConfig().getString("setLanguage") + ".noPerm")));
