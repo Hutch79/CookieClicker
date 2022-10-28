be3bf80b05fa96e30959b26c90e0d6f277de0c24
Hutch79 <andrin.monn@gmail.com>
2022-10-28 09:23:33 +0200

    Made CookieClickerCommand structure mor readable

--- src/main/java/ch/hutch79/cookieclicker/CookieClickerCommand.java
+++ src/main/java/ch/hutch79/cookieclicker/CookieClickerCommand.java
@@ -2,16 +2,14 @@ package ch.hutch79.cookieclicker;
 
 import ch.hutch79.cookieclicker.util.DatabaseManager;
 import org.bukkit.Bukkit;
-import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 import ch.hutch79.cookieclicker.util.Gui;
-
 import java.sql.SQLException;
 import java.util.Objects;
-import java.util.UUID;
 
 public class CookieClickerCommand implements CommandExecutor {
 
@@ -24,23 +22,28 @@ public class CookieClickerCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
-        if (sender instanceof Player) {
-            Player player = (Player) sender;
-            if (player.hasPermission("cookieclicker.use") || player.hasPermission("cookieclicker.admin")) {
-                try {
-                    DatabaseManager.updateUser(player, 0.0, 0.0, 0, 0.0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
-                    Gui.mainGui(player, 1);
-                } catch (SQLException e) {
-                    throw new RuntimeException(e);
-                }
-            } else {
-                player.sendMessage(Objects.requireNonNull(main.getConfig().getString("language." + main.getConfig().getString("setLanguage") + ".noPerm")));
-            }
-        } else {
+        if (sender instanceof ConsoleCommandSender) {
             Bukkit.getConsoleSender().sendMessage(Objects.requireNonNull(main.getConfig().getString("language." + main.getConfig().getString("setLanguage") + ".noPlayer")));
+            return false;
         }
 
-        return false;
+        if (args.length == 0) {
+            Main(sender);
+        }
+        return true;
     }
 
+    public void Main(CommandSender sender){
+        Player player = (Player) sender;
+        if (!player.hasPermission("cookieclicker.use") || !player.hasPermission("cookieclicker.admin")) {
+            player.sendMessage(Objects.requireNonNull(main.getConfig().getString("language." + main.getConfig().getString("setLanguage") + ".noPerm")));
+        }
+            try {
+                DatabaseManager.updateUser(player, 0.0, 0.0, 0, 0.0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+                Gui.mainGui(player, 1);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+
+    }
 }
